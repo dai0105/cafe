b771a37664a7faa95d03351a96e2fe1c4ceb815d
dai0105 <daiki4457@gmail.com>
2026-03-30 01:15:24 +0900

    update

--- guide/settings.py
+++ guide/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME'),
+]
 
 
 # Application definition
